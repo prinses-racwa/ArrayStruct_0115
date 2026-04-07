e7a89dde6bc67f90d1e98b5ae3e4a17754b69e6c
Rajwa Khoirunisa <rajwaamelia@gmail.com>
2026-04-07 18:14:54 +0700

    menampilkan data perulangan

--- Array.cpp
+++ Array.cpp
@@ -1,2 +1,43 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    // deklarasi array disertai inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
+
+    // deklarasi tanpa inisialisasi
+    int datamu[5];
+
+    // cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1] << endl;
+
+    // mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
+
+    // mengisi data dengan perintah satu persatu
+    cout << "Isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "Isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "Isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[4];
+
+     // mengisi data melalui perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
+
+    // menampilkan data dengan perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " : " << datamu[i] << endl;
+    }
+
+}``
